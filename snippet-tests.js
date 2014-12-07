dd97891eefd421625fa0555bdebbe94993fddcf0
Cesar Andreu <cesarandreu@gmail.com>
2014-12-07 03:17:51 -0800

    add js testing file

---
+++ snippet-tests.js
@@ -0,0 +1,64 @@
+// console
+ca
+
+cc
+
+cd
+
+ce
+
+cl
+
+ct
+
+
+// functions
+afn
+
+gfn
+
+fn
+
+vfn
+
+iifn
+
+
+// control flow
+if
+
+ife
+
+
+// iterations
+for
+
+fori
+
+forr
+
+forin
+
+
+// packages
+req
+
+
+// testing
+ae
+
+be
+
+desc
+
+ita
+
+itg
+
+its
+
+
+// other
+si
+
+st
